7183e4f364e13c14719a98f225f831e3001058ff
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-05-03 10:59:47 -0500

    Se crea toda la conexion al api getway

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,73 @@
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
+
+
+import { NATS_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+
+@Controller('orders')
+export class OrdersController {
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.client.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
+    return this.client.send('findAllOrders', orderPaginationDto);
+  }
+  
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe ) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.client.send('findOneOrder', { id })
+      );
+
+      return order;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+
+      return this.client.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe ) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    try {
+      return this.client.send('changeOrderStatus', { id, status: statusDto.status })
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+
+
+}
